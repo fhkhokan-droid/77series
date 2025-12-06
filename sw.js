9990b6941c1b30b98898d94815e69a72acf61b55
fhkhokan-droid <fhkhokan@gmail.com>
2025-12-06 13:13:38 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'directory-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './index.json',
+  './manifest.json',
+  './icon.png'
+  // You can add more static assets here if needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
